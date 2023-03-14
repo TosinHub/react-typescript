6bf3a661dd030b6bfb37a0e3001a4ddabad3b51a
TosinHub <tosinolugbenga@gmail.com>
2023-03-14 17:33:01 +0000

    Set up

---
+++ src/components/Greet.tsx
@@ -0,0 +1,10 @@
+
+export const Greet = () =>{
+    return (
+        <div>
+            <h2>
+                Welcome to Growth! You have 10 unread messages.
+            </h2>
+        </div>
+    )
+}
